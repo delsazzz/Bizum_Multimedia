c6c2ef48555199de8bc09e3441d0958e980cf370
delsazzz <diegodel27@gmail.com>
2025-11-18 20:08:07 +0100

    Acción del botón
    Código actualizado con los cambios que hacen que el botón envíe el mensaje de bizum enviado

--- app/src/main/java/com/example/bizum/MainActivity.kt
+++ app/src/main/java/com/example/bizum/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -23,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -55,6 +58,19 @@ fun BizumApp() {
 @Preview(showBackground = true)
 @Composable
 fun PantallaPrincipalBizum (modifier: Modifier = Modifier) {
+    var enviado by remember { mutableStateOf(false) } // Declaramos la variable enviado para usarla en el botón
+    // Abrimos esta box para meter todo el contenido dentro
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Esta imagen es el fondo de la pantalla y por eso la colocamos arriba del todo
+        Image(
+            painter = painterResource(R.drawable.fondo_bizum),
+            contentDescription = null, // Necesario aunque sea null para que funcione Image()
+            contentScale = ContentScale.Crop, // Esto hace que ocupe toda la pantalla
+            modifier = Modifier.fillMaxSize()
+        )
+
     // Column sirve para organizar el contenido de manera horizontal
     Column (
         modifier = modifier,
@@ -62,13 +78,13 @@ fun PantallaPrincipalBizum (modifier: Modifier = Modifier) {
     ) {
         // Función imagen para añadirla
         Image(
-            painter = painterResource(R.drawable.logo_bizum), // Añadir la imagen
+            painter = painterResource(R.drawable.logo_bizum_sin_fondo), // Añadir la imagen
             contentDescription = "Logo de la aplicación", // Agregar una descripción del contenido de la imagen
             modifier = Modifier.height(150.dp).width(150.dp) // Ajustar tamaño de la imagen
         )
         // Spacer agregar un espacio entre la imagen y el botón
         Spacer(
-            modifier = Modifier.height(100.dp)
+            modifier = Modifier.height(50.dp)
         )
         // Llamada a la función para el cuadro de texto donde añadiremos los contactos
         NombreContactoField()
@@ -77,17 +93,31 @@ fun PantallaPrincipalBizum (modifier: Modifier = Modifier) {
         )
         ImporteField()
         Spacer(
-            modifier = Modifier.height(100.dp)
+            modifier = Modifier.height(50.dp)
         )
         // Función botón para añadirlo
-        Button(onClick = { /*TODO*/ },
+        Button(
+            onClick = { enviado = true }, // Dentro del onClick definimos que funcione cuando la variable sea true
             modifier = Modifier.height(60.dp).width(120.dp),
             colors = ButtonDefaults.buttonColors(
                 containerColor = colorResource(id = R.color.rojo), // Color para el botón
-                contentColor = colorResource(R.color.white)) // Color para el texto del botón
-            ) {
+                contentColor = colorResource(R.color.white) // Color para el texto del botón
+            )
+        ) {
             Text(stringResource(R.string.Enviar))
-            }
+        }
+        // Este bucle ejecuta el texto si pulsamos el botón porque enviado está definido como true
+        if (enviado) {
+            Spacer(
+                modifier = Modifier.height(100.dp)
+            )
+            // Texto que aparece cuando pulsamos el botón de enviar
+            Text(
+                text = "¡Bizum enviado!",
+                color = colorResource(R.color.verde)
+            )
+        }
+    }
 
         }
     }
@@ -118,3 +148,4 @@ fun ImporteField(){
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number) // Esto permite que el teclado que aparezca sea solo numérico
     )
 }
+
